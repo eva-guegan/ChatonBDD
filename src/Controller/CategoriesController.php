d5c7b7099ffc76a2af8292cdfbf29039e0c7e47c
Eva Guegan <eva.guegan@hotmail.fr>
2019-11-14 11:26:06 +0100

    ajout / modif cat

--- src/Controller/CategoriesController.php
+++ src/Controller/CategoriesController.php
@@ -4,7 +4,9 @@ namespace App\Controller;
 
 use App\Entity\Categorie;
 use App\Form\CategorieType;
+use App\Repository\CategorieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CategoriesController extends AbstractController
@@ -12,26 +14,80 @@ class CategoriesController extends AbstractController
     /**
      * @Route("/categories", name="categories")
      */
-    public function index()
+    public function index(CategorieRepository $repository)
     {
+        $categories=$repository->findAll();
         return $this->render('categories/index.html.twig', [
-            'controller_name' => 'CategoriesController',
+            'categories'=>$categories
         ]);
     }
 
     /**
      * @Route("/categories/ajouter", name="categorie_ajouter")
      */
-    public function ajouter()
+    public function ajouter(Request $request)
     {
         $categorie = new Categorie();
 
         //Création form
         $formulaire=$this->createForm(CategorieType::class, $categorie);
 
+        //récup données POST
+        $formulaire->handleRequest($request);
+
+        //Enregistrer dans BDD
+        if ($formulaire->isSubmitted() && $formulaire->isValid()){
+            //récup de Entity manager
+            $em=$this->getDoctrine()->getManager();
+
+            //garder objet en BDD
+            $em->persist($categorie);
+
+            //execute l'insert
+            $em->flush();
+
+            //je m'en vais
+            return $this->redirectToRoute("categories");
+        }
+
         return $this->render('categories/formulaire.html.twig', [
             'formulaire'=>$formulaire->createView(),
             'h1'=>"Ajouter une catégorie"
+        ]);
+    }
+
+    /**
+     * @Route("/categories/modifier/{id}", name="categorie_modifier")
+     */
+    public function modifier(CategorieRepository $repository, $id, Request $request)
+    {
+        //chercher objet à modif
+        $categorie = $repository->find($id);
+
+        //Création form
+        $formulaire=$this->createForm(CategorieType::class, $categorie);
+
+        //récup données POST
+        $formulaire->handleRequest($request);
+
+        //Enregistrer dans BDD
+        if ($formulaire->isSubmitted() && $formulaire->isValid()){
+            //récup de Entity manager
+            $em=$this->getDoctrine()->getManager();
+
+            //garder objet en BDD
+            $em->persist($categorie);
+
+            //execute l'insert
+            $em->flush();
+
+            //je m'en vais
+            return $this->redirectToRoute("categories");
+        }
+
+        return $this->render('categories/formulaire.html.twig', [
+            'formulaire'=>$formulaire->createView(),
+            'h1'=>"Modifier la catégorie ".$categorie->getTitre()
         ]);
     }
 }
